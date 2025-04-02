c8bdf097b9d06ab0930fce74d3fe8661e82bb411
Ali Cebe <alicebe86@gmail.com>
2025-04-02 12:04:47 -0400

    Some code changing for Vercel deplayment

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,19 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: '**',
+    path: 'missions',
     renderMode: RenderMode.Prerender
+  },
+  {
+    path: '',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'mission/:flight_number',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: '**',
+    renderMode: RenderMode.Client
   }
 ];
